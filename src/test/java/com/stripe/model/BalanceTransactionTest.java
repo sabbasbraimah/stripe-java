15c9fea98869b3212a353b27ce192c1568e711d1
Mick Jermsurawong <mickjermsurawong@stripe.com>
2018-12-04 14:50:21 -0800

    fix test not fixture and rely on stripemock for expansion

--- src/test/java/com/stripe/model/BalanceTransactionTest.java
+++ src/test/java/com/stripe/model/BalanceTransactionTest.java
@@ -23,8 +23,10 @@ public class BalanceTransactionTest extends BaseStripeTest {
 
   @Test
   public void testDeserializeExpansions() throws Exception {
-    // TODO: Figure out why stripe-mock does not expand source when asked
-    final String data = getResourceAsString("/api_fixtures/balance_transaction_expansion.json");
+    final String[] expansions = {
+        "source",
+    };
+    final String data = getFixture("/v1/balance/history/txn_123", expansions);
     final BalanceTransaction resource = ApiResource.GSON.fromJson(data, BalanceTransaction.class);
     assertNotNull(resource);
     final HasId source = resource.getSourceObject();
